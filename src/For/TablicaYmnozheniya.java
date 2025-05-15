919f759e629fc1dff0304da2c86961b5b293a76f
vitja <vit.jabrailoff2016@yandex.ru>
2025-05-15 13:03:01 +0300

    ва

--- src/For/TablicaYmnozheniya.java
+++ src/For/TablicaYmnozheniya.java
@@ -7,11 +7,15 @@ public class TablicaYmnozheniya {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите цифру от 1 до 10");
         int n = scanner.nextInt();
-
         for (int i = 0; i <= 10; ){
+            if (n < 0 || n > 10) {
+                System.out.println("от 1 до 10");
+                break;
+            }
             i++;
             if (i == 10) break;
             int sum = n * i;
+
             System.out.println(n + "*" + i + " = " + sum);
         }
 
